7792e00badbe9a7389469e744a4c8e7ed0262fee
Habib <habibo5656@gmail.com>
2023-02-23 08:24:07 -0800

    Revert "Auth"
    This reverts commit fddb29657fdebda94b77898547a804399697d8b4.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,16 +13,6 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-
-Route::controller(IndexController::class)
-->middleware(['auth', 'verified'])->group(function(){
-    Route::get('/','index')->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
